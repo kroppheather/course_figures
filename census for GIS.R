1bad986bf64423b7e3a4671027eaabee6321184f
Heather Kropp <kroppheather@gmail.com>
2022-09-29 16:34:38 -0400

    Update census for GIS.R

--- census for GIS.R
+++ census for GIS.R
@@ -8,6 +8,9 @@ library(tidycensus)
 source("c:/Users/hkropp/Documents/census_key.r")
 census_api_key(key)
 
+
+##### Oneida county #####
+
 #total population, 2 white alone, 3, black alone , 4= native american alone, 5 = asian alone, 8 = two or more races
 race <- get_acs("tract", state="36", county="065",
                 variables = c("B02001_001","B02001_002", "B02001_003", "B02001_004", "B02001_005", "B02001_008"),
@@ -17,17 +20,90 @@ race <- get_acs("tract", state="36", county="065",
 income_house <- get_acs("tract", state="36", county="065",
                         variables = c("B19013_001"),year=2020, geometry=TRUE)
 plot(income_house["estimate"])
-income_housep <- st_transform(income_house,32618 )
 
 
-# vulnerable ages
+
+
+
 
 # column 1 is total people, 3 is male under 5 years and 27 is female under 5 years old
 age <- get_acs("tract", state="36", county="065",
-               variables = c("B01001_001","B01001_002","B01001_027"), year=2020, geometry=TRUE, output="wide")
+               variables = c("B01001_001","B01001_003","B01001_027"), year=2020, geometry=TRUE, output="wide")
 
 
-# poverty
 
-poverty <- get_acs("tract", state="36", county="065",
-                   variables = c("S1701_001"), year=2020, geometry=TRUE, output="wide",survey="acs1")
+# vulnerable ages (kids under 5)
+
+age$kid_total <- ((age$B01001_003E + age$B01001_027E)/ age$B01001_001E)*100
+plot(age["kid_total"])
+
+child <- data.frame(FID = seq(1,nrow(age)),
+                    GEOID = age$GEOID,
+                    chld_five = age$kid_total,
+                    geometry=age$geometry)
+
+child_oneida <- st_as_sf(child)
+
+### organize income data
+incomeSubset <- data.frame(FID = seq(1,nrow(income_house)),
+                           GEOID = income_house$GEOID,
+                           hh_income = income_house$estimate,
+                           margin_err = income_house$moe,
+                           geometry= income_house$geometry)
+income_oneida <- st_as_sf(incomeSubset)
+
+
+### organize race data
+
+# organize for students to use on ArcGIS online. Convert to proportions for choropleth
+
+raceSubset <- race[,c("B02001_001E","B02001_002E","B02001_003E", "B02001_004E", "B02001_005E", "B02001_008E")]
+
+raceSubset$white_pct  <- ifelse(raceSubset$B02001_001E == 0, 0,
+                                    (raceSubset$B02001_002E/raceSubset$B02001_001E)*100)
+plot(raceSubset["percent_white"])
+
+raceSubset$black_pct  <- ifelse(raceSubset$B02001_001E == 0, 0,
+                                    (raceSubset$B02001_003E/raceSubset$B02001_001E)*100)
+plot(raceSubset["percent_black"])
+
+raceSubset$ntv_am_pct  <- ifelse(raceSubset$B02001_001E == 0, 0,
+                                    (raceSubset$B02001_004E/raceSubset$B02001_001E)*100)
+
+plot(raceSubset["percent_native_american"])
+
+raceSubset$asn_pct  <- ifelse(raceSubset$B02001_001E == 0, 0,
+                                              (raceSubset$B02001_005E/raceSubset$B02001_001E)*100)
+
+plot(raceSubset["percent_asian"])
+
+raceSubset$multi_pct  <- ifelse(raceSubset$B02001_001E == 0, 0,
+                                    (raceSubset$B02001_008E/raceSubset$B02001_001E)*100)
+
+plot(raceSubset["percent_biracial"])
+
+
+# prepare for ArcGIS
+
+race_oneida <- data.frame(FID = seq(1, nrow(raceSubset)),
+                          GEOID = race$GEOID,
+                          raceSubset[,8:12],
+                          geometry = raceSubset$geometry)
+
+race_oneida <- st_as_sf(race_oneida)
+
+
+st_write(race_oneida, "E:/Google Drive/GIS/census/oneida/acs_2020_fiveyear/race_oneida_acs.shp")
+
+st_write(income_oneida, "E:/Google Drive/GIS/census/oneida/acs_2020_fiveyear/income_oneida_acs.shp")
+
+st_write(child_oneida, "E:/Google Drive/GIS/census/oneida/acs_2020_fiveyear/children_oneida_acs.shp")
+
+library(raster)
+noise <- raster("E:/Google Drive/GIS/dot_2018_noise/CONUS_rail_road_and_aviation_noise_2018/CONUS_rail_road_and_aviation_noise_2018/State_rasters/NY_rail_road_and_aviation_noise_2018.tif")
+plot(noise)
+st_crs(race_oneida)
+noise@crs
+noiseP <- projectRaster(noise, crs="+init=epsg:4269")
+noiseCrop <- crop(noiseP, race_oneida)
+
